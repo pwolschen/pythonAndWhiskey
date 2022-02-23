217ed1f4e0d9de969ddbf83fa4f4aca8d47904e5
Wolschendorf, Philip <wolschendorf_p@fev.local>
2022-02-23 20:24:15 +0100

    Added Ball object

--- Schwerkraftsimulation.py
+++ Schwerkraftsimulation.py
@@ -3,16 +3,20 @@ class ball():
     a = 0
     v = 0
 
-    def __init__(self):
-        self.height = 0
+    # def __init__(self):
+        # self.height = 0
         
-    def __init__(self, height):
+    def __init__(self, ballheight=0):
         self.a = 0
         self.v = 0
-        self.height = height
+        self.height = ballheight
         
-    
 
+def sim(ball, timestep):
+    ball.a = acceleration()
+    ball.v = velocity(ball.v, ball.a, timestep)
+    ball.height = displacement(ball.height, ball.v, timestep)
+    return ball
 
 def acceleration():
     return -9.81
@@ -26,10 +30,15 @@ def displacement(startingDisplacement, velocity, timestep):
 height = float(input("Drop height: "))
 timestep = .001
 
-ballA = ball(height)
+ballArr = []
+for i in range(0,100):
+    ballArr.append(ball(height))
+print(ballArr)
+print(ballArr[0])
+print(ballArr[0].height)
+#exit()
 v = 0
 while ballA.height > 0:
-    ballA.a = acceleration()
-    ballA.v = velocity(ballA.v, ballA.a, timestep)
-    ballA.height = displacement(ballA.height, ballA.v, timestep)
-    print("{},{},{}".format(a,v,height))
+    ballArr[0] = sim(ballArr[0], timestep)
+    ballArr[1] = sim(ballArr[1], timestep)
+    print("A: {},B: {}".format(ballArr[0].height, ballArr[1].height))
