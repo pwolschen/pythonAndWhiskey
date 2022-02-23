94257777bc386649e30dc9d62d44df317c31c460
Wolschendorf, Philip <wolschendorf_p@fev.local>
2022-02-23 14:08:28 +0100

    Added Ball Object

--- Schwerkraftsimulation.py
+++ Schwerkraftsimulation.py
@@ -1,4 +1,18 @@
 
+class ball():
+    a = 0
+    v = 0
+
+    def __init__(self):
+        self.height = 0
+        
+    def __init__(self, height):
+        self.a = 0
+        self.v = 0
+        self.height = height
+        
+    
+
 
 def acceleration():
     return -9.81
@@ -12,10 +26,10 @@ def displacement(startingDisplacement, velocity, timestep):
 height = float(input("Drop height: "))
 timestep = .001
 
-
+ballA = ball(height)
 v = 0
-while height > 0:
-    a = acceleration()
-    v = velocity(v, a, timestep)
-    height = displacement(height, v, timestep)
+while ballA.height > 0:
+    ballA.a = acceleration()
+    ballA.v = velocity(ballA.v, ballA.a, timestep)
+    ballA.height = displacement(ballA.height, ballA.v, timestep)
     print("{},{},{}".format(a,v,height))
